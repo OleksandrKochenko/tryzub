246f14f04ef18d4febbf2fa662b181b39a9ec0ad
OleksandrKochenko <121250212+OleksandrKochenko@users.noreply.github.com>
2024-05-18 23:39:26 +0300

    tailwind

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,14 @@
 export const App = () => {
   return (
     <div
+      className="bg-slate-600"
       style={{
         height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       Tryzub Web Page
